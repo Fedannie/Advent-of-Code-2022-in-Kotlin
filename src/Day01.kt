601f8f1a21a83cc322221976a62461110ede5df7
Anna Fedorova <anna.fedorova.se@gmail.com>
2022-12-01 21:54:50 +0100

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,41 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        var current = 0
+
+        for (i in input.indices) {
+            if (input[i].isBlank()) {
+                result = result.coerceAtLeast(current)
+                current = 0
+            } else {
+                current += Integer.parseInt(input[i])
+            }
+        }
+        return result.coerceAtLeast(current)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = ArrayList<Int>(0)
+
+        var current = 0
+        for (i in input.indices) {
+            if (input[i].isBlank()) {
+                calories.add(current)
+                current = 0
+            } else {
+                current += Integer.parseInt(input[i])
+            }
+        }
+        calories.add(current)
+        return calories.sortedDescending().subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    println(part2(testInput))
+
+    println()
 
     val input = readInput("Day01")
     println(part1(input))
