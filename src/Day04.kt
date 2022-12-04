739c561d1cb39bb418d8a8cd7c75ea7e8b699f9d
Anna Fedorova <anna.fedorova.se@gmail.com>
2022-12-04 13:02:55 +0100

    Day 04 solution

---
+++ src/Day04.kt
@@ -0,0 +1,25 @@
+fun main() {
+  fun parseInput(input: String): List<IntRange> {
+    return input.split(',')
+      .map { it.split('-').map { it.toInt() } }
+      .sortedWith(compareBy<List<Int>> { it[0] }.thenByDescending { it[1] - it[0] })
+      .map { it[0] .. it[1] }
+  }
+
+  fun part1(input: List<String>): Int {
+    return input.map(::parseInput).count { pair -> pair[1].last in pair[0] }
+  }
+
+  fun part2(input: List<String>): Int {
+    return input.map(::parseInput).count { pair -> pair[0].last >= pair[1].first }
+  }
+
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("Day04_test")
+  check(part1(testInput) == 2)
+  check(part2(testInput) == 4)
+
+  val input = readInput(4)
+  println(part1(input))
+  println(part2(input))
+}
