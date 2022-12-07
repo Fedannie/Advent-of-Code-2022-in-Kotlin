adabe9fb9fdcdf23a6fedd1e230b900f5c6703e6
Anna Fedorova <anna.fedorova.se@gmail.com>
2022-12-07 11:35:45 +0100

    refactor

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,22 +1,5 @@
 fun main() {
-  fun MutableMap<Char, Int>.dec(ch: Char) {
-    if ((this[ch] ?: 1) == 1) remove(ch)
-    else this[ch] = this[ch]!! - 1
-  }
-
-  fun MutableMap<Char, Int>.inc(ch: Char) {
-    this[ch] = (this[ch] ?: 0) + 1
-  }
-
-  fun countMarker(input: String, size: Int): Int {
-    val memory = input.subSequence(0, size).groupingBy { it }.eachCount().toMutableMap()
-    for (i in size until input.length) {
-      memory.dec(input[i - size])
-      memory.inc(input[i])
-      if (memory.size == size) return i + 1
-    }
-    return -1
-  }
+  fun countMarker(input: String, size: Int): Int = input.windowed(size).indexOfFirst { it.toSet().size == size } + size
 
   fun part1(input: String): Int = countMarker(input, 4)
 
